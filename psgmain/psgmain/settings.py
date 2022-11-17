a95f98c2310585a8112a9e2570ee4f62a8686796
KayChewe <chewe.kasonde@outlook.com>
2022-11-17 20:18:18 +0200

    database

--- psgmain/psgmain/settings.py
+++ psgmain/psgmain/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'analytics.apps.AnalyticsConfig', # Anlystics App Configuration Specification
 ]
 
 MIDDLEWARE = [
