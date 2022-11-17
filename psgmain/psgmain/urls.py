019d598f5b02cf7749257bc50f794d5dc86169a8
KayChewe <chewe.kasonde@outlook.com>
2022-11-17 21:20:37 +0200

    migtrations

--- psgmain/psgmain/urls.py
+++ psgmain/psgmain/urls.py
@@ -15,7 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from django.views.generic import RedirectView 
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('analytics/', include('analytics.urls')),
+    path('', RedirectView.as_view(url='analytics/', permanent=True))
 ]
+
+# Use static() to add URL mapping to serve static files during development (only)
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
